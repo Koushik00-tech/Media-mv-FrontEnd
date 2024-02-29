aa1f6e8f53cd10eddba01973a5c33cd894925e21
Koppolu Koushik <koushik.koppolu00@gmail.com>
2024-02-29 11:42:41 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from "./components/Navbar";
+import Sidebar from "./components/Sidebar";
+import Header from "./components/Header";
+import Live from "./components/Live";
+import "./App.css";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Sidebar />
+      <Navbar />
+      <div className="main-cont">
+        <Header />
+        <Live />
+      </div>
+    </>
   );
-}
+};
 
 export default App;
